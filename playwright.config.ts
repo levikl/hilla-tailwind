39e5d5cb4626aa390e8631c5fc237587fb4fcd4e
Levi Klingler <levikl@geeked.gg>
2026-05-27 17:32:58 -0400

    troubleshooting playwright in ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,14 +2,17 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "src/main/frontend/e2e",
+  expect: { timeout: 30_000 },
   use: {
     baseURL: "http://localhost:8080",
+    screenshot: "only-on-failure",
+    trace: "on-first-retry",
   },
   webServer: {
     command: "./gradlew bootRun --args='--vaadin.launch-browser=false'",
     url: "http://localhost:8080",
     timeout: 120_000,
-    reuseExistingServer: false,
+    reuseExistingServer: !process.env.CI,
   },
   projects: [
     {
